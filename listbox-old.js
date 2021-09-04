1692266760248273d87e1a66a38c566664d60304
hamilsauce <jake@seahag.onmicrosoft.com>
2021-09-04 10:45:40 -0700

    fixed

---
+++ listbox-old.js
@@ -0,0 +1,98 @@
+class Listbox {
+  constructor(root, listEl, itemEls) {
+    this.root = root;
+    this.listEl = listEl;
+    this.itemEls = itemEls;
+    this._activeItem;
+
+    // this.listEl.addEventListener('click', this.handleClick)
+  }
+
+  handleClick({ target }) {
+    this.activeItem = target;
+    console.log('targo', target.textContent);
+  }
+
+  get activeItem() {
+    return this._activeItem;
+  }
+  set activeItem(newVal) {
+    this._activeItem = newVal;
+  }
+}
+
+const app = document.querySelector('#app');
+const listbox = document.querySelector('.listbox');
+const list = document.querySelector('.listbox-list');
+const listItems = document.querySelectorAll('.listbox-item');
+
+const lister = new Listbox(listbox, list, listItems)
+
+console.log('lisyet', lister);
+
+const handleTouchDown = e => {
+  console.log('rouch down', e.target);
+  e.target.draggable = true;
+  e.target.addEventListener('touchend', handleTouchEnd)
+}
+const handleTouchEnd = e => {}
+const startTouch = e => {}
+
+listItems.forEach(item => {
+  // 	item.addEventListener('touchstart', handleTouchDown)
+  item.addEventListener('touchstart', handleTouchDown)
+})
+
+
+let dragged;
+
+/* events fired on the draggable target */
+document.addEventListener("drag", e => {
+  console.log('e in drag evt', e)
+}, true);
+
+document.addEventListener("dragstart", function(event) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+  console.log('drag s');
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function(event) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.classList.contains('listbox-list')) {
+    event.target.style.background = "purple";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.classList.contains('listbox-list')) {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if (event.target.classList.contains('listbox-list')) {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
+  }
+}, false);
--- listbox.js
+++ listbox.js
@@ -1,98 +1,197 @@
-class Listbox {
-	constructor(root, listEl, itemEls) {
-		this.root = root;
-		this.listEl = listEl;
-		this.itemEls = itemEls;
-		this._activeItem;
+class DragItem {
+  constructor(rootElement = undefined, data = {}, parent = undefined) {
+    this.root = rootElement || this.createRootElement();
 
-		this.listEl.addEventListener('click', this.handleClick)
-	}
+    this.setEventHandlers();
 
-	handleClick({ target }) {
-		this.activeItem = target;
-		console.log('targ', target.textContent);
-	}
+  }
 
-	get activeItem() {
-		return this._activeItem;
-	}
-	set activeItem(newVal) {
-		this._activeItem = newVal;
-	}
+  createRootElement() {
+    const el = document.createElement('li');
+    el.classList.add('listbox-item');
+    el.setAttribute('draggable', 'true');
+    el.textContent = 'Add txt content';
+    return el;
+  }
+  setEventHandlers() {
+    this.root.addEventListener('dragstart', e => {
+      e.dataTransfer.setData('text/plain', null)
+
+    });
+  }
 }
 
-const app = document.querySelector('#app');
-const listbox = document.querySelector('.listbox');
-const list = document.querySelector('.listbox-list');
-const listItems = document.querySelectorAll('.listbox-item');
+class DragContainer {
+  constructor(element, containerSelector = '.drag-container', childSelector = '.drag-item', children = []) {
+    this.el = element;
+    this.childSelector = childSelector;
+    this.containerSelector = containerSelector;
+    this._children = this.el.children;
+    this._dragItems = new Map(this.children
+      .reduce((acc, curr, i) => [...acc, Object.values(this.createItem(curr, {
+        text: `New Item ${i}`,
+        id: i
+      }))], []));
+    this._dragged;
+    this._dropTarget;
+    this.draggedStartIndex;
+    this.dropTargetIndex;
+    this.setEventHandlers();
+  }
 
-const lister = new Listbox(listbox, list, listItems)
+  createItem(el, data = {}, shouldMapItem = false) {
+    const newItem = {
+      containerElement: this.el,
+      dragContainer: this,
+      data: Object.keys(data).length !== 0 ? data : { text: el.textContent }
+    };
 
-console.log('lisyet', lister);
+    el.textContent = newItem.data.text
+    el.setAttribute('draggable', 'true');
+    el.classList.add('listbox-item');
 
-const handleTouchDown = e => {
-	console.log('rouch down', e.target);
-	e.target.draggable = true;
-	e.target.addEventListener('touchend', handleTouchEnd)
+    if (shouldMapItem === true) this.dragItems.set(el, newItem);
+    this.el.appendChild(el);
+    return { el, data: newItem };
+  }
+
+  resetItems() {}
+  renderItems() {}
+
+  get children() { return [...this._children] }
+  set children(newVal) { this._children = newVal }
+
+  get dragItems() { return this._dragItems }
+  set dragItems(newVal) { this._dragItems = newVal }
+
+  get dragged() { return this._dragged }
+  set dragged(newVal) {
+    this.draggedStartIndex = this.children.findIndex(el => el === newVal);
+    this._dragged = newVal
+  }
+
+  get dropTarget() { return this._dropTarget }
+  set dropTarget(newVal) {
+    this.dropTargetIndex = this.children.findIndex(el => el === newVal);
+    this._dropTarget = newVal
+  }
+
+  get className() { return this.containerSelector.startsWith('.') ? this.containerSelector.replace('.', '') : 'drag-container' }
+  get childClassName() { return this.childSelector.startsWith('.') ? this.childSelector.replace('.', '') : 'drag-item' }
+
+  eachElement(rootElement, callback) {
+    if (false === callback(rootElement)) return false;
+    if (!callback) {
+      const elements = [];
+      eachElement(rootElement, element => elements.push(element));
+      return elements;
+    }
+    if (rootElement.children.length > 0) {
+      const elements = rootElement.children
+      for (let i = 0, l = elements.length; i < l; ++i) {
+        if (false === eachElement(elements[i], callback)) {
+          return;
+        }
+      }
+    }
+  }
+
+  setEventHandlers() {
+    /* events fired on the draggable target */
+    this.el.addEventListener("drag", e => {}, false);
+
+    this.el.addEventListener("dragstart", e => {
+      e.dataTransfer.setData('text/plain', e.target.id);
+      this.dragged = e.target;
+      this.dragged.style.opacity = 0.3;
+    }, true);
+
+    this.el.addEventListener("dragend", e => {
+      e.preventDefault();
+      e.stopPropagation();
+      e.target.style.opacity = "";
+      // this.dragged.classList.remove('dragging')
+      // e.target.classList.remove('dragged-over-in')
+      // e.target.classList.remove('dragged-over-out')
+
+    }, true);
+
+    /* events fired on the drop targets */
+    this.el.addEventListener("dragover", e => {
+      e.preventDefault();
+      e.stopPropagation();
+
+    });
+    this.el.addEventListener("dragenter", e => {
+      const targ = e.target;
+      if (targ.classList.contains(this.childClassName)) {
+        // targ.classList.add('dragged-over')
+        targ.style.background = "blue"
+      };
+    }, true);
+
+    // @@DRAG ENTER FOR DRAG ITEMS
+    this.el.querySelectorAll(this.childSelector)
+      .forEach(el => el.addEventListener("dragenter", e => {
+        const targ = e.target;
+        // targ.classList.add('dragged-over-in')
+        targ.style.background = "blue";
+      }, true));
+
+    this.el.querySelectorAll(this.childSelector)
+      .forEach(el => el.addEventListener("click", e => {
+        const targ = e.target;
+      }, true));
+
+    this.el.addEventListener("dragleave", e => {
+      const targ = e.target;
+      if (targ.classList.contains(this.childClassName)) {
+        // targ.classList.remove('dragged-over-in')
+        // targ.classList.add('dragged-over-out')
+        // targ.classList.remove('dragged-over-out')
+        targ.style.background = ""
+      };
+
+      if (targ.classList.contains(this.childClassName)) {
+        targ.style.background = "";
+      }
+    }, true);
+
+    this.el.addEventListener("drop", e => {
+      // prevent default action (open as link for some elements)
+      e.preventDefault();
+      e.stopPropagation();
+      const targ = e.target;
+      targ.classList.remove('dragged-over')
+      this.dropTarget = targ;
+      if (targ.classList.contains(this.childClassName)) {
+        targ.style.background = "";
+
+        if (this.dragged.isSameNode(targ)) {
+          return;
+        } else if (this.draggedStartIndex > this.dropTargetIndex) {
+          this.dragged.parentElement.removeChild(this.dragged);
+          targ.parentElement.insertBefore(this.dragged, targ);
+        } else if (this.dragged.parentElement !== targ.parentElement) {
+          this.dragged.parentElement.removeChild(this.dragged);
+          targ.parentElement.insertBefore(this.dragged, targ);
+        } else {
+          this.dragged.parentElement.removeChild(this.dragged);
+          targ.parentElement.insertBefore(this.dragged, targ.parentElement.children[this.dropTargetIndex]);
+        }
+      }
+
+      if (targ.classList.contains(this.className)) {
+        console.log('drop targ  = parent');
+        targ.style.background = "";
+        this.dragged.parentElement.removeChild(this.dragged);
+        targ.appendChild(this.dragged);
+      }
+
+      this.el.focused = true;
+      this.el.focus();
+      this.dragged = null;
+      this.dropTarget = null;
+    }, true);
+  }
 }
-const handleTouchEnd = e => {}
-const startTouch = e => {}
-
-// listItems.forEach(item => {
-// 	item.addEventListener('touchstart', handleTouchDown)
-// 	item.addEventListener('touchstart', handleTouchDown)
-// })
-
-
-let dragged;
-
-/* events fired on the draggable target */
-document.addEventListener("drag", function(event) {
-
-}, false);
-
-document.addEventListener("dragstart", function(event) {
-	// store a ref. on the dragged elem
-	dragged = event.target;
-	// make it half transparent
-	event.target.style.opacity = .5;
-	console.log('drag s');
-}, false);
-
-document.addEventListener("dragend", function(event) {
-	// reset the transparency
-	event.target.style.opacity = "";
-}, false);
-
-/* events fired on the drop targets */
-document.addEventListener("dragover", function(event) {
-	// prevent default to allow drop
-	event.preventDefault();
-}, false);
-
-document.addEventListener("dragenter", function(event) {
-	// highlight potential drop target when the draggable element enters it
-	if (event.target.classList.contains('listbox-list')) {
-		event.target.style.background = "purple";
-	}
-
-}, false);
-
-document.addEventListener("dragleave", function(event) {
-	// reset background of potential drop target when the draggable element leaves it
-	if (event.target.classList.contains('listbox-list')) {
-		event.target.style.background = "";
-	}
-
-}, false);
-
-document.addEventListener("drop", function(event) {
-	// prevent default action (open as link for some elements)
-	event.preventDefault();
-	// move dragged elem to the selected drop target
-	if (event.target.classList.contains('listbox-list')) {
-		event.target.style.background = "";
-		dragged.parentNode.removeChild(dragged);
-		event.target.appendChild(dragged);
-	}
-}, false);
